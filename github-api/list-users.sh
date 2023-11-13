253be30e43db12a4449cf455a884aec667cf1348
phanichandar09 <150455313+phanichandar09@users.noreply.github.com>
2023-11-13 18:58:59 -0500

    Update list-users.sh
    Added helper class

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -35,6 +38,15 @@ function list_users_with_read_access {
         echo "$collaborators"
     fi
 }
+
+
+function helper{
+expected_cmd_args=2
+if [$# -ne $expected_cmd_args]; then
+echo "please execute the script with required cmd args"
+echo "asd"
+}
+
 
 # Main script
 
